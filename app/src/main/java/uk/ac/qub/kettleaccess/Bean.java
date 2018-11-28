82362fd1b50f7afe0dc0d74083cb7b7f55b5ed0e
DanielDavidSteele <DanielSteele120809@gmail.com>
2018-11-28 18:41:09 +0000

    Some commenting has been completed

--- app/src/main/java/uk/ac/qub/kettleaccess/Bean.java
+++ app/src/main/java/uk/ac/qub/kettleaccess/Bean.java
@@ -1,6 +1,15 @@
 package uk.ac.qub.kettleaccess;
 
+/**
+ * Class to define the parameters of the object "Bean"
+ */
+
 public class Bean {
+
+    /**
+     * Declaration of private variables to be used
+     * as the parameters of the bean object
+     */
 
     private String Name;
     private String Origin;
@@ -8,8 +17,25 @@ public class Bean {
     private String Description;
     private int Image;
 
+
+    /**
+     * A required public constructor
+     */
+
     public Bean() {
     }
+
+
+    /**
+     * Constructor used to pass the parameters of the bean
+     * object to other classes in the project
+     *
+     * @param name
+     * @param origin
+     * @param temp
+     * @param description
+     * @param image
+     */
 
     public Bean(String name, String origin, int temp, String description, int image){
         Name = name;
@@ -20,47 +46,92 @@ public class Bean {
 
     }
 
-    //Getter
-
+    /**
+     * Getter for the name parameter
+     * @return name
+     */
 
     public String getName() {
         return Name;
     }
 
+    /**
+     * Getter for the origin parameter
+     * @return
+     */
+
     public String getOrigin() {
         return Origin;
     }
+
+    /**
+     * Getter for the temperature parameter
+     * @return
+     */
 
     public int getTemp() {
         return Temp;
     }
 
+    /**
+     * Getter for the description parameter
+     * @return
+     */
+
     public String getDescription(){
         return Description;
     }
+
+    /**
+     * Getter for the image parameter
+     * @return
+     */
 
     public int getImage(){
         return Image;
     }
 
-    //Setter
 
+    /**
+     * Setter for the name parameter
+     * @param name
+     */
 
     public void setName(String name) {
         Name = name;
     }
 
+    /**
+     * Setter for the origin parameter
+     * @param origin
+     */
+
     public void setOrigin(String origin) {
         Origin = origin;
     }
+
+    /**
+     * Setter for the temperature parameter
+     * @param temp
+     */
 
     public void setTemp(int temp) {
         Temp = temp;
     }
 
+    /**
+     * Setter for the description parameter
+     * @param description
+     */
+
     public void setDescription(String description){
         Description = description;
     }
+
+    /**
+     * Setter for the image parameter
+     * @param image
+     */
 
     public void setImage (int image){
         Image = image;
--- app/src/main/java/uk/ac/qub/kettleaccess/BeanFragment.java
+++ app/src/main/java/uk/ac/qub/kettleaccess/BeanFragment.java
@@ -3,18 +3,25 @@ package uk.ac.qub.kettleaccess;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.ViewGroup;
-import android.view.View;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Public class BeanFragment extends the functionality of the
+ * Fragment class in the Android library
+ */
+
 public class BeanFragment extends Fragment {
+
+    /**
+     * Declaration of variables
+     */
 
     View v;
     private RecyclerView myRecyclerView;
@@ -44,13 +51,43 @@ public class BeanFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
+        /*final RequestQueue queue = Volley.newRequestQueue(getContext());
+        String url = ("http://10.6.1.143:8081");
+        final Map<String, String> map = new HashMap<String, String>();
+        StringRequest stringRequest;
+        stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //System.out.println(response);
+                String[] responses = response.split("#");
+                int i = 0;
+                for(String str:responses) {
+                    String cut = str.substring(6, str.length()-2);
+                    map.put("Name"+i, cut);
+                    //System.out.println(map.get("Name"));
+                }
+
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        System.out.println("No luck");
+                    }
+                });
+        queue.add(stringRequest);*/
+
+
+
        lstBean = new ArrayList<>();
 
         //loop for time-saving while building
 
         for(int count = 0; count < 100; count ++){
-            lstBean.add(new Bean("True and Honest", "El Salvador", count, "This is a bean description", R.drawable.el_salvador));
+            lstBean.add(new Bean("Frank and Honest", "El Salvador", count, "This is a bean description", R.drawable.el_salvador));
         }
+
     }
 }
 
