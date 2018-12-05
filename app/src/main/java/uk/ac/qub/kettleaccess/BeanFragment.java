2f52f890ada16302ce06c1ba36159c67ee10cbc9
DanielDavidSteele <DanielSteele120809@gmail.com>
2018-12-05 11:25:53 +0000

    Update
    Update 11.25am 05/12/2018

--- app/src/main/java/uk/ac/qub/kettleaccess/BeanFragment.java
+++ app/src/main/java/uk/ac/qub/kettleaccess/BeanFragment.java
@@ -9,8 +9,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Public class BeanFragment extends the functionality of the
@@ -27,46 +36,76 @@ public class BeanFragment extends Fragment {
     private RecyclerView myRecyclerView;
     private List<Bean> lstBean;
 
+    /**
+     * Required public constructor
+     */
+
     public BeanFragment(){
 
     }
 
+    /**
+     * OnCreateView method that boots the UI of the fragment
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return
+     */
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        /*
+          Inflates the view "v" with the layout of "fragment_bean.xml"
+          to the size of the container
+         */
         v = inflater.inflate(R.layout.fragment_bean, container, false);
 
+        //Instantiate recycler view
         myRecyclerView = (RecyclerView) v.findViewById(R.id.bean_recyclerView);
 
+        //Instantiate recycler view adapter
         RecyclerViewAdapter recyclerAdapter = new RecyclerViewAdapter(getContext(), lstBean, this);
+
+        //Setting the layout manager for the view
         myRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        //Setting the adapter as the recycler view adapter
         myRecyclerView.setAdapter(recyclerAdapter);
 
+        //Returns the view "v"
         return v;
     }
 
+    /**
+     * OnCreate method that sets the business logic behind the fragment
+     * @param savedInstanceState
+     */
 
-
-     @Override
+    @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
 
-        /*final RequestQueue queue = Volley.newRequestQueue(getContext());
-        String url = ("http://10.6.1.143:8081");
+
+        final RequestQueue queue = Volley.newRequestQueue(getContext());
+        String url = ("http://10.6.1.143/get.php");
         final Map<String, String> map = new HashMap<String, String>();
         StringRequest stringRequest;
         stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
-                //System.out.println(response);
-                String[] responses = response.split("#");
+               System.out.println(response);
+                /*String[] responses = response.split("#");
                 int i = 0;
                 for(String str:responses) {
-                    String cut = str.substring(6, str.length()-2);
-                    map.put("Name"+i, cut);
-                    //System.out.println(map.get("Name"));
-                }
+                    //String cut = str.substring(6, str.length()-2);
+                    map.put("Name"+i, str);
+                    System.out.println(map.get("Name"));
+                }*/
+                System.out.println(response);
+
 
             }
         },
@@ -76,9 +115,10 @@ public class BeanFragment extends Fragment {
                         System.out.println("No luck");
                     }
                 });
-        queue.add(stringRequest);*/
+        queue.add(stringRequest);
 
 
+        //Instantiation of the list of bean objects
 
        lstBean = new ArrayList<>();
 
